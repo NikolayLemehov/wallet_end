23e4713606f55c21f8e8ab26d88c72c0c45803ad
EgorRybin <antoniopareiro@gmail.com>
2023-01-26 00:59:24 +0200

    clean code

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,34 +1,18 @@
-const express = require('express');
-// const ctrl = require('../../controllers/users/index.js');
-const { validation, auth } = require('../../middlewares');
-// const { users } = require('../../models/index.js');
+const express = require("express");
 
-const ctrl = require('../../controllers').transactions;
-const { transactions } = require('../../models');
+const { validation, auth } = require("../../middlewares");
+const ctrl = require("../../controllers").transactions;
+
+const { transactions } = require("../../models");
 
 const router = express.Router();
 
-// router.post(
-//   '/transactions',
-//   validation(users.registerJoiSchema),
-//   ctrl.register,
-// );
+router.get("/", auth, ctrl.getAll);
 
-// router.get('/transactions/:id', validation(users.loginJoiSchema), ctrl.login);
+router.post("/", auth, validation(transactions.addTransJoiSchema), ctrl.add);
 
-// router.get('/transactions/:id/month', auth, ctrl.getCurrent);
+router.get("/categories", auth);
 
-// router.post('/transactions/:id/year', auth, ctrl.logout);
-
-// router.patch('/', auth, validation(users.subscriptionJoiSchema), ctrlWrapper(ctrl.updateSubscription));
-
-router.get('/', auth, ctrl.getAll);
-
-router.post ('/', auth, validation(transactions.addTransJoiSchema), ctrl.add);
-
-router.get('/categories', auth);
-
-router.get('/statistic', auth);
-
+router.get("/statistic", auth);
 
 module.exports = router;
