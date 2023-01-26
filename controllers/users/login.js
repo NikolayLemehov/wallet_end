563f081686843dd9a6393d1034f57734e6574331
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-01-26 15:31:16 +0200

    fix: token expired time equal 100h

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -16,7 +16,7 @@ const login = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "100h" });
 
   const updatedUser = await User.findByIdAndUpdate(
     user._id,
